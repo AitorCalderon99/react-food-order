c17321bf0adf05900ddf7fccfa760dfc0758f1d2
Aitor Calderon <aitorclzamakola@gmail.com>
2025-12-31 14:34:00 +0100

    feat(Checkout): Handling form submission, validation and sending a basic POST request to BACKEND

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -16,6 +16,26 @@ export default function Checkout() {
     }, 0)
   )
 
+  function handleSubmit(event) {
+    event.preventDefault()
+
+    const fd = new FormData(event.target)
+    const customerData = Object.fromEntries(fd.entries())
+
+    fetch('http://localhost:3000/orders', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        order: {
+          items: cartContext.items,
+          customer: customerData,
+        },
+      }),
+    })
+  }
+
   return (
     <Modal
       open={userProgressContext.progress === 'checkout'}
@@ -25,25 +45,27 @@ export default function Checkout() {
           : null
       }
     >
-      <h2>Checkout</h2>
-      <p>Total amount: {cartTotal}</p>
-      <Input label="Full name" type="text" id="full-name" />
-      <Input label="Email" type="email" id="email" />
-      <Input label="Street" type="text" id="street" />
-      <div className="control-row">
-        <Input label="Postal code" type="text" id="postal-code" />
-        <Input label="City" type="text" id="city" />
-      </div>
-      <p className="modal-actions">
-        <Button
-          type="button"
-          textOnly
-          onClick={userProgressContext.hideCheckout}
-        >
-          Close
-        </Button>
-        <Button>Submit order</Button>
-      </p>
+      <form onSubmit={handleSubmit}>
+        <h2>Checkout</h2>
+        <p>Total amount: {cartTotal}</p>
+        <Input label="Full name" type="text" id="name" />
+        <Input label="Email" type="email" id="email" />
+        <Input label="Street" type="text" id="street" />
+        <div className="control-row">
+          <Input label="Postal code" type="text" id="postal-code" />
+          <Input label="City" type="text" id="city" />
+        </div>
+        <p className="modal-actions">
+          <Button
+            type="button"
+            textOnly
+            onClick={userProgressContext.hideCheckout}
+          >
+            Close
+          </Button>
+          <Button>Submit order</Button>
+        </p>
+      </form>
     </Modal>
   )
 }
